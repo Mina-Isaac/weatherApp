4d19769d55b20324843f658403e8e015ff02837d
Mina Isaac <mina.isaac.2000@gmail.com>
2020-10-20 01:49:21 +0200

    Updates

---
+++ precache-manifest.024582f12ddd9c752f7a61791cda70ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6487c2ae07be508624c7f6ed9686d8",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "7fdecd89470fe5449cef",
+    "url": "/weatherApp/static/css/main.8bee715c.chunk.css"
+  },
+  {
+    "revision": "724dcd6ec96bf5a05702",
+    "url": "/weatherApp/static/js/2.0d0527e7.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/weatherApp/static/js/2.0d0527e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdecd89470fe5449cef",
+    "url": "/weatherApp/static/js/main.1c4ce339.chunk.js"
+  },
+  {
+    "revision": "efba474b3fd5a4a7aa5f",
+    "url": "/weatherApp/static/js/runtime-main.d4919a9c.js"
+  }
+]);
